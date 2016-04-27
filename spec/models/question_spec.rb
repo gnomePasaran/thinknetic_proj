3ddc7395c36124c1604835cbb7e3302b29ae8276
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-04-27 14:04:37 +0300

    add test for deleting

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,13 +4,21 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 
-  it 'should has many answers' do
-    question = FactoryGirl.create(:question)
-    answer_1 = FactoryGirl.create(:answer)
-    answer_2 = FactoryGirl.create(:answer)
-    question.answers << answer_1
-    question.answers << answer_2
+  before(:each) do
+    @question = FactoryGirl.create(:question)
+    @answer_1 = FactoryGirl.create(:answer)
+    @answer_2 = FactoryGirl.create(:answer)
+    @question.answers << @answer_1
+    @question.answers << @answer_2
+  end
 
-    expect(question.answers.count).to eq 2
+  it 'should has many answers' do
+    expect(@question.answers.count).to eq 2
+  end
+
+  it 'should delete all related answers' do
+    @question.destroy
+
+    expect(Answer.count).to eq 0
   end
 end
