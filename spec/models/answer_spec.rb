d5f37ac11dc0bb4533893a50da04ea0d8c1d11a2
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-04-28 01:06:39 +0300

    add fix for tests and dependent

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,12 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :body }
-
-  it 'should belongs to the one question' do
-    answer = FactoryGirl.create(:answer)
-    question = FactoryGirl.create(:question)
-    question.answers << answer
-
-    expect(answer.question).to eq question
-  end
+  it { should belong_to(:question) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,22 +3,5 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
-  before(:each) do
-    @question = FactoryGirl.create(:question)
-    answer_1 = FactoryGirl.create(:answer)
-    answer_2 = FactoryGirl.create(:answer)
-    @question.answers << answer_1
-    @question.answers << answer_2
-  end
-
-  it 'should has many answers' do
-    expect(@question.answers.count).to eq 2
-  end
-
-  it 'should delete all related answers' do
-    @question.destroy
-
-    expect(Answer.count).to eq 0
-  end
+  it { should have_many(:answers).dependent(:destroy) }
 end
