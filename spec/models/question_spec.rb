995bd90e457cf1a8673898a059c7edb977116ea9
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-04-27 11:19:54 +0300

    add test for assocciation for Question and Answer

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,14 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  it 'should has many answers' do
+    question = FactoryGirl.create(:question)
+    answer_1 = FactoryGirl.create(:answer)
+    answer_2 = FactoryGirl.create(:answer)
+    question.answers << answer_1
+    question.answers << answer_2
+
+    expect(question.answers.count).to eq 2
+  end
 end
