2aa007719a28dfc60f7c7db101b08506ada6b290
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-04 18:03:45 +0300

    add users tests

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  In order to be able to singed out
+  As an User
+  I want to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Signed in user try to sign out' do
+    sign_in(user)
+    click_on 'Logout'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+end
---
+++ spec/acceptance/user_registrations_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User registration', %q{
+  In order to be able to registrate
+  As an User
+  I want to be able to registrate
+} do
+
+  scenario 'User try to registrate' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+end
