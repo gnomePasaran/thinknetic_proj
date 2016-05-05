7e78f5b53af42b427d9181badd92afca850f1389
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-05 11:14:04 +0300

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
+  has_many :questions, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :answers
-  has_many :questions
 end
