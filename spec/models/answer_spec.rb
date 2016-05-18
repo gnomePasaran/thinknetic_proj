14f74352b867d8cb1e6c819a077037c075446b8e
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-18 17:52:32 +0300

    add acceptence test for deleting

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -10,5 +10,5 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:user) }
 
   it { should have_many(:attachments).dependent(:destroy) }
-  it { should accept_nested_attributes_for :attachments }
+  it { should accept_nested_attributes_for(:attachments).allow_destroy(true) }
 end
