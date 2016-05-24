9b6a42a2f4a0b91fa93206ad432ea4171979275d
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-24 17:56:15 +0300

    add model concern for vote

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,11 +1,11 @@
 class Answer < ActiveRecord::Base
+  include Votable
+
   belongs_to :question
   belongs_to :user
 
   has_many :attachments, as: :attachable, dependent: :destroy
   accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
-
-  has_many :votes, as: :votable, dependent: :destroy
 
   validates :question_id, :body, :user_id, presence: true
 
@@ -17,22 +17,22 @@ class Answer < ActiveRecord::Base
     end
   end
 
-  def vote(user, score)
-    if vote = self.votes.find_by_user_id(user)
-      if Vote.respond_to?(score) && Vote.scores.to_s.include?(score)
-        vote.public_send("#{score}!")
-      end
-    else
-      vote = self.votes.create(user: user, score: score)
-    end
-    vote
-  end
+  # def vote(user, score)
+  #   if vote = self.votes.find_by_user_id(user)
+  #     if Vote.respond_to?(score) && Vote.scores.to_s.include?(score)
+  #       vote.public_send("#{score}!")
+  #     end
+  #   else
+  #     vote = self.votes.create(user: user, score: score)
+  #   end
+  #   vote
+  # end
 
-  def get_score
-    self.votes.sum(:score)
-  end
+  # def get_score
+  #   self.votes.sum(:score)
+  # end
 
-  def get_vote(user)
-    self.votes.find_by_user_id(user)
-  end
+  # def get_vote(user)
+  #   self.votes.find_by_user_id(user)
+  # end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  include Votable
+
   belongs_to :user
 
   has_many :answers, dependent: :destroy
@@ -7,26 +9,25 @@ class Question < ActiveRecord::Base
   has_many :attachments, as: :attachable, dependent: :destroy
   accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
-  has_many :votes, as: :votable, dependent: :destroy
-
   validates :title, :body, :user_id, presence: true
 
-  def vote(user, score)
-    if vote = self.votes.find_by_user_id(user)
-      if Vote.respond_to?(score) && Vote.scores.to_s.include?(score)
-        vote.public_send("#{score}!")
-      end
-    else
-      vote = self.votes.create(user: user, score: score)
-    end
-    vote
-  end
+  # def vote(user, score)
+  #   # p model_name.name.demodulize.constantize
+  #   if vote = self.votes.find_by_user_id(user)
+  #     if Vote.respond_to?(score) && Vote.scores.to_s.include?(score)
+  #       vote.public_send("#{score}!")
+  #     end
+  #   else
+  #     vote = self.votes.create(user: user, score: score)
+  #   end
+  #   vote
+  # end
 
-  def get_score
-    self.votes.sum(:score)
-  end
+  # def get_score
+  #   self.votes.sum(:score)
+  # end
 
-  def get_vote(user)
-    self.votes.find_by_user_id(user)
-  end
+  # def get_vote(user)
+  #   self.votes.find_by_user_id(user)
+  # end
 end
