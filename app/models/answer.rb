d4711bc649030e7236daf5984c04366e3108d00c
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-24 15:10:45 +0300

    add assocciates for models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -16,4 +16,23 @@ class Answer < ActiveRecord::Base
       save!
     end
   end
+
+  def vote(user, score)
+    if vote = self.votes.find_by_user_id(user)
+      if Vote.respond_to?(score) && Vote.scores.to_s.include?(score)
+        vote.public_send("#{score}!")
+      end
+    else
+      vote = self.votes.create(user: user, score: score)
+    end
+    vote
+  end
+
+  def get_score
+    self.votes.sum(:score)
+  end
+
+  def get_vote(user)
+    self.votes.find_by_user_id(user)
+  end
 end
---
+++ spec/acceptance/vote_for_answer_spec.rb
@@ -0,0 +1,78 @@
+require_relative 'acceptance_helper'
+
+feature 'Vote for the answer', %q{
+  In order to be able to vote for the answer
+  As an User
+  I want to be able to vote for the answer
+} do
+
+  given!(:author) { create(:user) }
+  given!(:non_author) { create(:user) }
+  given!(:question) { create(:question, user: author) }
+  given!(:answer) { create(:answer, question: question, user: author) }
+
+  describe 'Non-author of answer ' do
+    before do
+      sign_in(non_author)
+      visit question_path question
+    end
+
+    scenario 'try to vote', js: true do
+      within "#answer-#{answer.id}" do
+        expect(page).to have_link 'Vote +'
+        click_on 'Vote +'
+      end
+      within "#answer-#{answer.id}" do
+        expect(page).not_to have_content 'Vote +'
+        expect(page).to have_content 'Score: 1'
+        expect(page).to have_content 'Cancel'
+      end
+
+      within "#answer-#{answer.id}" do
+        expect(page).to have_link 'Cancel'
+        click_on 'Cancel'
+      end
+      within "#answer-#{answer.id}" do
+        expect(page).not_to have_content 'Cancel'
+        expect(page).to have_content 'Score: 0'
+        expect(page).to have_content 'Vote +'
+      end
+
+      within "#answer-#{answer.id}" do
+        expect(page).to have_link 'Vote -'
+        click_on 'Vote -'
+      end
+      within "#answer-#{answer.id}" do
+        expect(page).not_to have_content 'Vote -'
+        expect(page).to have_content 'Score: -1'
+        expect(page).to have_content 'Cancel'
+      end
+    end
+  end
+
+  describe 'Author of question' do
+    before do
+      sign_in(author)
+      visit question_path question
+    end
+
+    scenario 'try to vote for his question', js: true do
+      within "#answer-#{answer.id}" do
+        expect(page).to have_content 'Score:'
+        expect(page).not_to have_link 'Vote +'
+        expect(page).not_to have_link 'Cancel'
+        expect(page).not_to have_link 'Vote -'
+      end
+    end
+  end
+
+  scenario 'Non-authentificated user try to vote', js: true do
+    visit question_path question
+
+    within "#answer-#{answer.id}" do
+      expect(page).not_to have_content 'Vote +'
+      expect(page).not_to have_link 'Cancel'
+      expect(page).not_to have_link 'Vote -'
+    end
+  end
+end
