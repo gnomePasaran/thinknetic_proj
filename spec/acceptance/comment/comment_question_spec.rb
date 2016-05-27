000c904adb76cad465abf6fd9c29c8612c9dea9a
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-27 17:44:34 +0300

    add acceptance spec for comments

--- spec/acceptance/comment/comment_question_spec.rb
+++ spec/acceptance/comment/comment_question_spec.rb
@@ -7,42 +7,48 @@ feature 'Add comment', %q{
 } do
 
   given(:user) { create(:user) }
-  given(:not_user) { create(:user) }
   given!(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: user) }
 
-  scenario 'Authentificated user comment the question with valid', js: true do
-    sign_in(user)
-    visit question_path question
-
-    within '.question' do
-      expect(page).to have_content 'Add comment'
-      click_on 'Add comment'
-    end
-    within '.comment' do
-      fill_in 'Your comment', with: 'Question comment'
-      click_on 'Save comment'
+  describe 'Authentificated user to comment' do
+    background do
+      sign_in(user)
+      visit question_path question
     end
 
-    within '.question .comment' do
-      expect(page).to have_content 'Question comment'
-      expect(page).to have_link 'Edit'
-      expect(page).to have_link 'Delete'
+    scenario 'the question with valid', js: true do
+      within '.question' do
+        expect(page).to have_content 'Create comment'
+        click_on 'Create comment'
+      end
+      within '.question-comments' do
+        fill_in 'Your comment', with: 'Question comment'
+        click_on 'Create comment'
+      end
+
+      within '.question' do
+        expect(page).to have_content 'Question comment'
+      end
+    end
+
+    scenario 'the answer with valid', js: true do
+      within "#answer-#{answer.id}" do
+        expect(page).to have_content 'Create comment'
+        click_on 'Create comment'
+      end
+      within ".answer-comments" do
+        fill_in 'Your comment', with: 'Question comment'
+        click_on 'Create comment'
+      end
+
+      within "#answer-#{answer.id}" do
+        expect(page).to have_content 'Question comment'
+      end
     end
   end
 
-  # scenario 'Non-authentificated user answer the question', js: true do
-  #   visit questions_path
-  #   click_on question.title
-
-  #   expect(page).not_to have_selector 'textarea'
-  # end
-
-  # scenario 'Non-authentificated user answer the question', js: true do
-  #   sign_in(user)
-
-  #   visit question_path question
-  #   click_on 'Create answer'
-
-  #   expect(page).to have_content 'Body can\'t be blank'
-  # end
+  scenario 'Non-authentificated user try to comment', js: true do
+    visit question_path question
+    expect(page).not_to have_content 'Create comment'
+  end
 end
