8e7ffd60e528454699bc79c68b2e5e021cceef0b
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-05-31 01:24:38 +0300

    add responders to questions_comtroller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,48 +3,44 @@ class QuestionsController < ApplicationController
 
   before_action :load_question, only: [:edit, :show, :update, :destroy, :access]
   before_action :access, only: [:update, :destroy]
-
+  after_action  :publicate_question, only: :create
+  
   include Voted
 
+  respond_to :js
+
   def index
-    @questions = Question.all
+    respond_with(@questions = Question.all)
   end
 
   def show
     @answer = @question.answers.build
-    @answer.attachments.build
+    respond_with(@answer.attachments.build)
   end
 
   def new
-    @question = current_user.questions.new
-    @question.attachments.build
-  end
-
-  def edit
+    respond_with(@question = current_user.questions.new)
   end
 
   def create
-    @question = Question.new(question_params)
-    @question.user = current_user
-
-    if @question.save
-      PrivatePub.publish_to '/questions', question: @question.to_json
-      redirect_to @question
-    else
-      render :new
-    end
+    @question = Question.create(question_params.merge(user_id: current_user.id))
+    respond_with(@question)
   end
 
   def update
     @question.update(question_params)
+    respond_with @question
   end
 
   def destroy
-    @question.destroy
-    redirect_to questions_path
+    respond_with(@question.destroy)
   end
 
   private
+
+  def publicate_question
+    PrivatePub.publish_to '/questions', question: @question.to_json if @question.valid?
+  end
 
   def load_question
     @question = Question.find(params[:id])
