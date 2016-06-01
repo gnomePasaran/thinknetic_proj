3a46fcc451bea7a68b04b0b336922f6be3497ff2
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-06-02 01:09:24 +0300

    add configs

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+  	p "~~~~~~~~~~", request.env['omniauth.auth']
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if user.persisted?
-      sign_in_and_redirect @user, event: authentication
-      set_flash_message( :notice, :success, kind: 'Facebook') if navigational_format?
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook')# if navigational_formats?
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controller: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root to: 'questions#index'
 
   concern :votable do
