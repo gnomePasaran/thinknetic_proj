c56a694aad79ccdc63d3355553966451cc6b4203
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-06-20 16:00:52 +0300

    add controllers

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :question_params, only: :create
+
   def index
     @questions = Question.all
     respond_with @questions
@@ -6,7 +8,18 @@ class Api::V1::QuestionsController < Api::V1::BaseController
 
   def show
     @question = Question.find(params[:id])
-    p @question.comments
     respond_with @question
+  end
+
+  def create
+    authorize Question
+    @question = Question.create(question_params.merge(user_id: current_user.id))
+    respond_with @question
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
