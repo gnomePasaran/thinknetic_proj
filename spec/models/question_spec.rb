75f9f434463ab67a6ee2f999a7aaa75bcf786c87
Alexander Ivahnenko <alexander_i@fast-report.com>
2016-06-22 11:12:36 +0300

    add shared examples for models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :user_id }
   it { should belong_to(:user) }
 
-  it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:answers).dependent(:destroy).order(is_best: :desc, created_at: :asc) }
 
   it { should have_many(:attachments).dependent(:destroy) }
   it { should accept_nested_attributes_for(:attachments).allow_destroy(true) }
